ce19e1b9d4e20e3bbb09276c64b5be31fd9ae7b2
Hien <s3877996@rmit.edu.vn>
2022-12-29 23:52:36 +0700

    Resolve conflict

--- app/src/main/java/rmit/ad/myapplication/LoginActivity.java
+++ app/src/main/java/rmit/ad/myapplication/LoginActivity.java
@@ -1,183 +1,42 @@
 package rmit.ad.myapplication;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
-    String userID;
-
     EditText emailEditText, passwordEditText;
     Button loginBtn;
-    ImageButton googleBtn, facebookBtn;
     ProgressBar processBar;
-    TextView registerTextView,resetTextView;
-    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-    FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-
-    GoogleSignInOptions gso;
-    GoogleSignInClient gsc;
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        updateUI(user);
-    }
-
+    TextView registerTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        gsc = GoogleSignIn.getClient(this,gso);
 
-        //add components
         emailEditText = (EditText) findViewById(R.id.email);
         passwordEditText = (EditText) findViewById(R.id.password);
         loginBtn = (Button) findViewById(R.id.loginBtn);
-        googleBtn = (ImageButton) findViewById(R.id.Google_Button);
-        facebookBtn = (ImageButton) findViewById(R.id.Facebook_Button);
         registerTextView = (TextView) findViewById(R.id.register);
-        resetTextView = (TextView) findViewById(R.id.reset);
         processBar = (ProgressBar) findViewById(R.id.progress_bar);
 
-
-        googleBtn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                 Google_SignIn();
-            }
-        });
-
-        resetTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText resetMail = new EditText(view.getContext());
-                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(view.getContext());
-                passwordResetDialog.setTitle("Reset Password");
-                passwordResetDialog.setMessage("Enter your email to receive the recover link");
-                passwordResetDialog.setView(resetMail);
-
-                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //extract the email and send recovery link
-                        String email = resetMail.getText().toString();
-                        firebaseAuth.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                Utility.showToast(LoginActivity.this,"The reset link is sent to your email");
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Utility.showToast(LoginActivity.this,"The link has not been sent"+e.getLocalizedMessage());
-                            }
-                        });
-                    }
-                });
-
-                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //close the dialog
-                    }
-                });
-
-                passwordResetDialog.create().show();
-            }
-        });
         loginBtn.setOnClickListener((v)-> UserLogin());
         registerTextView.setOnClickListener((v)->startActivity(new Intent(LoginActivity.this,RegisterActivity.class)));
     }
 
-    private void Google_SignIn() {
-        Intent intent = gsc.getSignInIntent();
-        startActivityForResult(intent,1000);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == 1000){
-                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-                try {
-                    GoogleSignInAccount account = task.getResult(ApiException.class);
-                    firebaseAuthWithGoogle(account.getIdToken());
-
-                } catch (ApiException e) {
-                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
-                }
-        }
-    }
-
-    //Authentication (FIREBASE) via Google
-    private void firebaseAuthWithGoogle(String idToken) {
-                    AuthCredential credential = GoogleAuthProvider.getCredential(idToken,null);
-                    firebaseAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(task.isSuccessful()){
-                                FirebaseUser user = firebaseAuth.getCurrentUser();
-                                Toast.makeText(LoginActivity.this, user.getEmail(), Toast.LENGTH_SHORT).show();
-                                updateUI(user);
-                            } else {
-                                Toast.makeText(LoginActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
-                                updateUI(null);
-                            }
-                        }
-                    });
-    }
-
-    private void updateUI(FirebaseUser user) {
-        //finish();
-        if(user!=null){
-            Intent intent = new Intent(getApplicationContext(),MainActivity.class);         //when merging change to MainActivity();
-            startActivity(intent);}
-
-    }
 
     void UserLogin(){
         String email = emailEditText.getText().toString();
@@ -191,6 +50,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     void loginAccountInFireBase(String email, String password){
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         changeInProgress(true);
         firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
@@ -199,8 +59,8 @@ public class LoginActivity extends AppCompatActivity {
                 if(task.isSuccessful()){
                     //email has been register and verify
                     if(firebaseAuth.getCurrentUser().isEmailVerified()){
-                        //go to main activity (for now go to profile to test)
-                        startActivity(new Intent(LoginActivity.this,Profile.class));
+                        //go to main activity
+                        startActivity(new Intent(LoginActivity.this,MainActivity.class));
                         finish();
                     }else{
                         Utility.showToast(LoginActivity.this,"Email has not been verified");
--- app/src/main/java/rmit/ad/myapplication/MainActivity.java
+++ app/src/main/java/rmit/ad/myapplication/MainActivity.java
@@ -1,95 +1,14 @@
 package rmit.ad.myapplication;
 
-import static android.content.ContentValues.TAG;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    String userID;
-//    ImageView menu = (ImageView)findViewById(R.id.menu);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Get the data from Google account
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if(account != null) {
-            String google_name = account.getDisplayName();
-            String google_mail = account.getEmail();
-            createAccountInFireBase(google_name,google_mail);
-        }
-
-    }
-
-
-    // check if the data of this google user is exits - then no need to create the data in cloud firestore
-    // else create a new user using google fullname and email
-    private void createAccountInFireBase(String google_name, String google_mail) {
-
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-        userID = firebaseAuth.getUid();
-        //https://firebase.google.com/docs/firestore/query-data/get-data#get_a_document
-
-        DocumentReference documentReference = firestore.collection("user").document(userID);
-
-        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful()){
-                    DocumentSnapshot documentSnapshot = task.getResult();
-                    if(documentSnapshot.exists()){
-                        //do nothing
-                    } else {
-                        userID = firebaseAuth.getUid();
-                        DocumentReference documentReference = firestore.collection("user").document(userID);
-                        Map<String, Object> user = new HashMap<>();
-                        user.put("email", google_mail);
-                        user.put("full_name", google_name);
-                        user.put("dob", null);
-                        user.put("phone_number", null);
-                        user.put("address", null);
-                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                Log.d("TAG", "onSuccess: user profile is created for" + userID);
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.d(TAG, "onFailure: " + e.toString());
-                            }
-                        });
-                    }
-                } else {
-                    Log.d(TAG, "onFailure: ");
-                }
-            }
-        });
-
-
     }
 }
