3bd47922e1b2741c548719c472cabb67667bb84b
Dustin461 <s3802674@student.rmit.edu.au>
2022-12-30 02:17:52 +0700

    Create class Item()

---
+++ app/src/main/java/rmit/ad/myapplication/ModelClass/Item.java
@@ -0,0 +1,99 @@
+package rmit.ad.myapplication.ModelClass;
+
+import android.graphics.Bitmap;
+
+public class Item {
+    private String ID;
+    private String name;
+    private String category;
+    private double price;
+    private double discount;
+    private String description;
+    private int stockQuantity;
+    private Bitmap image;
+    private String color;
+
+    public Item(String ID, String name, String category, double price, double discount, String description, int stockQuantity, Bitmap image, String color) {
+        this.ID = ID;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.discount = discount;
+        this.description = description;
+        this.stockQuantity = stockQuantity;
+        this.image = image;
+        this.color = color;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
