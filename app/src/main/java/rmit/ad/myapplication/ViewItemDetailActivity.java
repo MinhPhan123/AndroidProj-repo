cef9c0b629ab3322f1a4e36a74a562de93260079
Anh Pham <S3802674@student.rmit.edu.au>
2023-01-17 23:40:36 +0700

    Create ViewItemDetailActivity.java

---
+++ app/src/main/java/rmit/ad/myapplication/ViewItemDetailActivity.java
@@ -0,0 +1,14 @@
+package rmit.ad.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewItemDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_item_detail);
+    }
+}
